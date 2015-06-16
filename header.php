7989d1e96e4dbc4efa37a45f842587b65bc41f16
carlo-fontanos <carlo_zzz@yahoo.com>
2015-06-16 14:31:32 +0800

    Spaced out a few line of codes

--- header.php
+++ header.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <meta charset = "<?php bloginfo( 'charset' ); ?>" />
+    <meta name = "viewport" content = "width=device-width" />
     
 	<title><?php global $page; wp_title('|', true, 'right'); bloginfo( 'name' ); ?></title>
 
-    <link rel="SHORTCUT ICON" href="favicon.ico" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/bootstrap-theme.css" />
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel = "SHORTCUT ICON" href = "favicon.ico" />
+	<link rel = "stylesheet" type = "text/css" href = "<?php bloginfo('template_url'); ?>/css/bootstrap.css" />
+	<link rel = "stylesheet" type = "text/css" href = "<?php bloginfo('template_url'); ?>/css/bootstrap-theme.css" />
+	<link rel = "stylesheet" type = "text/css" media = "all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel = "pingback" href = "<?php bloginfo( 'pingback_url' ); ?>" />
 	
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/bootstrap.min.js"></script>
+	<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type = "text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 
     <?php
 		wp_head();
 		global $header_options;
-		if (!empty($header_options)) {
-			foreach ($header_options as $key => $value) { $$key = $value; }
-		}
+		if (!empty($header_options)) { foreach ($header_options as $key => $value) { $$key = $value; } }
     ?>
 </head>
 
