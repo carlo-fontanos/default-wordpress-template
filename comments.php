119cc57b0b3ba97fca3da812a151d085c348f38f
carlo-fontanos <carlo_zzz@yahoo.com>
2015-06-16 14:29:46 +0800

    Removed comment blocks

--- comments.php
+++ comments.php
@@ -1,13 +1,8 @@
-<?php // The Template for Comments ?>
-
 <div id="comments">
 	<?php if ( post_password_required() ) : ?>
     <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'wordpress' ); ?></p>
-</div><!-- comments -->
-<?php
-		/* Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have to fully load the template
-		 */
+</div>
+<?php		
 		return;
 	endif;
 ?>
@@ -23,8 +18,8 @@
         <div class="navigation">
             <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'wordpress' ) ); ?></div>
             <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'wordpress' ) ); ?></div>
-        </div> <!-- .navigation -->
-    <?php endif; //check for comment navigation ?>
+        </div>
+    <?php endif; ?>
 
 	<ol class="commentlist">
 		<?php wp_list_comments( array( 'callback' => 'twentythirteen_comment' ) ); ?>
@@ -34,20 +29,17 @@
         <div class="navigation">
             <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'wordpress' ) ); ?></div>
             <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'wordpress' ) ); ?></div>
-        </div><!-- .navigation -->
-    <?php endif; //check for comment navigation ?>
+        </div>
+    <?php endif; ?>
 
 <?php else : 
-	/* If there are no comments and comments are closed,
-	 * let's leave a little note, shall we?
-	 */
 	if ( ! comments_open() ) :
 ?>
 	<p class="nocomments"><?php _e( 'Comments are closed.', 'wordpress' ); ?></p>
-<?php endif; //end ! comments_open() ?>
+<?php endif; ?>
 
-<?php endif; //end have_comments() ?>
+<?php endif; ?>
 
 <?php comment_form(); ?>
 
-</div><!-- #comments -->
+</div>
