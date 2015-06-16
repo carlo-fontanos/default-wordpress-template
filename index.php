e1e00b2d5a1062ab80be431f449b2209915b43f3
carlo-fontanos <carlo_zzz@yahoo.com>
2015-06-16 14:31:56 +0800

    Added template structure as default index

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?php 
-	// The Main Template 
-	get_header(); 
-?>
+<?php get_header(); ?>
+
+	<section class = "inner-page-wrapper">
+		<section class = "container">
+			<section class = "row content">
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<h1><?php the_title(); ?></h1>
+						<article class="entry-content">
+							<?php the_content(); ?>
+						</article>
+					</article>
+				<?php endwhile; ?>
+			</section>
+		</section>
+	</section>
 	
-	<div id="content">
-    	<?php get_template_part( 'content', 'index' ); ?>
-	</div><!-- end content -->
-
-	<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
