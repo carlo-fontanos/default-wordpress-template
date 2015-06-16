aa0f40d67a9419c43f15bc792e8c3a5d8d901e58
carlo-fontanos <carlo_zzz@yahoo.com>
2015-06-16 14:23:46 +0800

    Removed unused section

--- 404.php
+++ 404.php
@@ -2,12 +2,10 @@
 
 	<section class = "inner-page-wrapper">
 		<section class = "container">
-			<section class  ="col-md-12">
-				<section class = "row content">				
-					<h1><?php _e( "Epic 404 - Article Not Found", "wordpress" ); ?></h1>
-					<p><?php _e( "The article you were looking for was not found, but maybe try searching below!", "wordpress" ); ?></p>
-					<?php get_search_form(); ?>
-				</section>
+			<section class = "row content">				
+				<h1><?php _e( 'Epic 404 - Article Not Found', 'cvftheme' ); ?></h1>
+				<p><?php _e( 'The article you were looking for was not found, but maybe try searching below!', 'cvftheme' ); ?></p>
+				<?php get_search_form(); ?>
 			</section>
 		</section>
 	</section>
