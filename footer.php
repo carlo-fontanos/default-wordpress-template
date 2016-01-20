3cbecaed65852afce4b6de4cad99c9e304a12f6c
Carlo Fontanos <carlo_zzz@yahoo.com>
2016-01-20 18:52:57 +0800

    Included helper css classes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <section class = "footer-wrapper">
 	<section class = "container">
 		<section class  ="col-md-12">
-			<section class = "row footer">
+			<section class = "row footer p-d">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 				<cite><?php _e('Copyright', 'cvftheme'); ?> &copy; <a href="<?php echo home_url('/'); ?>"><?php bloginfo( 'name' ); ?></a>, <?php echo date('Y'); ?>. <?php _e('All Rights Reserved', 'cvftheme'); ?>.</cite>
 			</section>
